2baa31bf45ac83a5521ce600658bb95cef3d0b41
kiki <christinemachhour@gmail.com>
2026-02-04 13:54:36 +0200

    feat: split portfolio sections into components and configure public assets

--- class-work/session1_angular/src/app/app.ts
+++ class-work/session1_angular/src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/navbar/navbar';
+import { AboutComponent } from './features/about/about';
+import { Projects } from './features/projects/projects';
+import { Profile } from './features/profile/profile';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, AboutComponent, Projects, Profile],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
